e3994b334408505506f09f37256baa4771a37466
loures <loures.raso@gmail.com>
2019-06-17 15:45:24 +0200

    Improve testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,50 +1,48 @@
 #!/usr/bin/env bash
 
+if [ ! -e "/tmp/objstore.sock" ]; then
+    echo "Server non attivo"
+    exit 1
+fi
+
+
 WORDS=$(shuf -n50 /usr/share/dict/italian)
 
-for line in $WORDS; do 
-    ./test $line 1  >> testout &
+function testclient {
+    ./test $1 $2 &
     wait $!
-done
+    if [ -f $? ]; then
+        echo "Client PID $!: Test $2 non passato"
+        return 1
+    else
+        echo "Client PID $!: Test $2 passato"
+    fi
+}
 
-if [ $(wc -l testout | cut -d" " -f1) -gt 0 ]; then
-    rm testout
-    echo "Test 1 non passato"
-    exit 1
-else
-    echo "Test 1 passato"
-fi
-
-rm testout
-
-for line in $WORDS; do 
-    ./test $line 2  >> testout &
-    wait $!
-done
-
-if [ $(wc -l testout | cut -d" " -f1) -gt 0 ]; then
-    cat testout
-    rm testout
-    echo "Test 2 non passato"
-    exit 1
-else
-    echo "Test 2 passato"
-fi
-
-rm testout
+#for line in $WORDS; do 
+#    ./test $line 1  >> testout &
+#    wait $!
+#done
+#
+#if [ $(wc -l testout | cut -d" " -f1) -gt 0 ]; then
+#    rm testout
+#    echo "Test 1 non passato"
+#    exit 1
+#else
+#    echo "Test 1 passato"
+#fi
+#
+#rm testout
 
 for line in $WORDS; do 
-    ./test $line 3  >> testout &
-    wait $!
+    testclient $line 1  
 done
 
-if [ $(wc -l testout | cut -d" " -f1) -gt 0 ]; then
-    cat testout
-    rm testout
-    echo "Test 3 non passato"
-    exit 1
-else
-    echo "Test 3 passato"
-fi
+for line in $WORDS; do 
+    testclient $line 2  
+done
 
-rm testout
+for line in $WORDS; do 
+    testclient $line 3 
+done
+
